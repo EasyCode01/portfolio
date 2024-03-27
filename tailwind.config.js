a86995ad226ca8b7554ffcbc6e5d04e4754d5130
EasyCode <ezekielayodele1@gmail.com>
2024-03-27 18:13:57 +0100

    Add: Add font-family and colors customization in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./build/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+      },
+      colors: {
+        "medium-gray": "#FAFAFA",
+        "title-color": "#333333",
+        "title-color-dark": "#262626",
+        "dark-medium-gray": "#262626",
+        "deep-medium-gray": "#B3B3B3",
+        "text-white-dark": "#F2F2F2",
+      },
+    },
   },
   plugins: [],
 };
